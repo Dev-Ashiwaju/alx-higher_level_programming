241042cea7888ebeb0e3931c5db79abe5c9ab1ee
Issa Saheed <olizsurelove@gmail.com>
2022-11-29 19:20:10 -0800

    push at once

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+i = 0
+while i <= 99:
+
+    print("0{0:d}".format(i) if i < 10 else "{0:d}".format(i),
+          end=", " if i < 99 else "\n")
+
+    i += 1
---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+i = 0
+j = 0
+
+while i < 10:
+    j = 0
+    while j < 10:
+        if (i != j and i < j):
+            print("{0:d}{1:d}".format(i, j), end="")
+            if (i >= 8 and j >= 9):
+                print()
+            else:
+                print(end=", ")
+        j += 1
+    i += 1
